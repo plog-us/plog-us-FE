6431e672f523c41e97d52d9a164a21dabd4ab986
Seunghyo <shyo0218@naver.com>
2024-02-16 16:56:32 +0900

    feat: finsh page

--- lib/app/view/map_page/finish_page.dart
+++ lib/app/view/map_page/finish_page.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:plog_us/app/view/theme/app_colors.dart';
 
 class FinishScreen extends StatelessWidget {
   final String locationName;
@@ -17,16 +19,61 @@ class FinishScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Finish Screen'),
-      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('플로깅 위치: $locationName'),
+            Container(
+              width: double.infinity,
+              padding: const EdgeInsets.all(12.0),
+              child: Image.asset(
+                'assets/images/check.png', // 로고 이미지 경로
+                width: 100, // 이미지 너비
+                height: 100, // 이미지 높이
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              '플로깅이 종료되었습니다! ',
+              style: TextStyle(
+                fontSize: 30,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Text(
+                '플로깅 위치: $locationName',
+                style: const TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ),
             Text(
-                '플로깅 시간: ${stopwatch.elapsed.inMinutes.toString().padLeft(2, '0')}:${(stopwatch.elapsed.inSeconds % 60).toString().padLeft(2, '0')}'),
+              '플로깅 시간: ${stopwatch.elapsed.inMinutes.toString().padLeft(2, '0')}:${(stopwatch.elapsed.inSeconds % 60).toString().padLeft(2, '0')}',
+              style: const TextStyle(
+                fontSize: 20,
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            TextButton(
+              onPressed: () {
+                Get.toNamed('/main');
+              },
+              child: const Text(
+                '메인으로 돌아가기',
+                style: TextStyle(
+                  fontSize: 17,
+                  color: AppColors.blueOrigin,
+                ),
+              ),
+            ),
           ],
         ),
       ),
